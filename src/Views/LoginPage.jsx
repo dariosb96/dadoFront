51b7bf6afea8675d933dfee201d303c43de759d6
dariosb96 <xdsb.tstx@gmail.com>
2026-02-16 19:45:24 -0600

    login

--- src/Views/LoginPage.jsx
+++ src/Views/LoginPage.jsx
@@ -46,14 +46,14 @@ const Login = () => {
       <img
         src={logo}
         alt="Logo"
-        className="w-32 h-auto object-contain"
+        className="w-12 h-auto object-contain"
       />
     </div>
       <div className="content max-w-md mx-auto p-6 bg-black bg-opacity-75 rounded-lg shadow-md">
 
        
 
-        <h1 className="text-white text-2xl font-semibold mb-6 py-4 text-center">Iniciar sesión</h1>
+        <h1 className="text-white text-xl font-semibold mb-6 py-2 text-center">Iniciar sesión</h1>
 
         <div className="form-group space-y-4">
           {error && <p className="text-red-500">{error}</p>}
@@ -61,10 +61,10 @@ const Login = () => {
           <form onSubmit={handleSubmit} className="space-y-4">
             <input
               type="email"
-              placeholder="Email"
+              placeholder="Correo electronico"
               value={useremail}
               onChange={(e) => setUserEmail(e.target.value)}
-              className="w-full bg-gray-900 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 text-gray-400 focus:ring-purple-500 focus:border-purple-500 shadow-sm"
+              className="w-full bg-gray-900 border border-black rounded-lg px-4 py-2 focus:outline-none focus:ring-2 text-gray-400 focus:ring-purple-500 focus:border-purple-500 shadow-sm"
             />
 
             <input
@@ -72,7 +72,7 @@ const Login = () => {
               placeholder="Password"
               value={userpassword}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full bg-gray-900 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 text-gray-400 focus:ring-purple-500 focus:border-purple-500 shadow-sm"
+              className="w-full bg-gray-900 border border-black rounded-lg px-4 py-2 focus:outline-none focus:ring-2 text-gray-400 focus:ring-purple-500 focus:border-purple-500 shadow-sm"
             />
 <div className="flex items-center gap-2 text-gray-400">
   <input
@@ -82,7 +82,7 @@ const Login = () => {
     onChange={(e) => setRememberMe(e.target.checked)}
     className="accent-purple-600"
   />
-  <label htmlFor="rememberMe" className="text-sm cursor-pointer">
+  <label htmlFor="rememberMe" className="text-xs cursor-pointer">
     Mantener sesión iniciada
   </label>
 </div>
@@ -90,7 +90,7 @@ const Login = () => {
             <button
               type="submit"
               disabled={loading}
-              className="w-full bg-purple-800 hover:bg-gray-500 text-white font-bold py-2 rounded-lg transition duration-300"
+              className="w-full bg-purple-800 hover:bg-gray-500 text-white font-semibold py-1 rounded-lg transition duration-300"
             >
               {loading ? 'Logging in...' : 'Ingresar'}
             </button>
@@ -98,13 +98,12 @@ const Login = () => {
 
           <div className="mt-5 text-center">
             <Link to="/create">
-              <h2 className="pb-16">Registrar</h2>
+              <h2 className="pb-10">Registrar</h2>
             </Link>
             <div className="mt-1 py-1">
               <Link to="/allcatalogs">
                 <h2 className="text-gray-500  font-semibold">
-                  ➜ Ver catálogos públicos ‼️
-                </h2>
+                  ➜ Ver catálogos públicos                 </h2>
               </Link>
             </div>
           </div>
